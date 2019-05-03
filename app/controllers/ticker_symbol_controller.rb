3f2a3f18e369f375687da2a783e9433d7c5b9c86
Joseph <jappleton@csumb.edu>
2019-05-03 09:09:04 +0000

    helper to draw haml symbol cards, add about to TS controller

--- app/controllers/ticker_symbol_controller.rb
+++ app/controllers/ticker_symbol_controller.rb
@@ -1,7 +1,13 @@
 class TickerSymbolController < ApplicationController
   def index
+    @ticker = TickerSymbol.all
   end
 
   def show
+  end
+  
+  def about
+    require "github/markup"
+    @readme = GitHub::Markup.render("README.md", File.read("README.md")).html_safe
   end
 end
