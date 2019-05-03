3065bcfcba1c71b44fd96d5d47f6b1bf05cebee5
Joseph <jappleton@csumb.edu>
2019-05-03 09:08:17 +0000

    add gems for markdown conversoin, haml, bootstrap, jquery

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,6 @@ Rails.application.routes.draw do
   #get 'ticker_symbol/show'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :ticker_symbol, only: [:index, :show]
+  get 'about', to: 'ticker_symbol#about'
   root 'ticker_symbol#index'
 end
