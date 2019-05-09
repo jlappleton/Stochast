a8dbd7d37f475a95aa71e4d2e625c897c3002e74
Joseph <jappleton@csumb.edu>
2019-05-09 07:10:24 +0000

    created display page, stubbed out models

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :ticker_symbol, only: [:index, :show]
   get 'about', to: 'ticker_symbol#about'
+  get 'prediction/:symbol/:task_id', to: 'prediction#show'
   root 'ticker_symbol#index'
 end
