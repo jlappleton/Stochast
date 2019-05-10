ac028ee51b3865f104a30e8ed2416cc18fe6094a
Joseph <jappleton@csumb.edu>
2019-05-10 08:23:38 +0000

    add flash alerts

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -3,15 +3,18 @@ class AdminController < ApplicationController
     auth_response = request.env["omniauth.auth"]
     if Admin.check_for_access(auth_response)
       session[:logged_in] = "true"
+      flash["alert-info"] = "Login Successful"
     end
     redirect_to("/dashboard")
   end
   def logout
     reset_session
+    flash["alert-danger"] = "You have logged out."
     redirect_to("/")
   end
   def dash
     if session[:logged_in].nil?
+      flash["alert-danger"] = "Nope."
       redirect_to("/")
     end
   end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,14 @@
 module ApplicationHelper
+  def flash_message
+    flash.each do |alert_type, message|
+        concat(content_tag(:div, message, class: "alert #{alert_type} alert-dismissible", role: 'alert') do
+            concat(content_tag(:button, class: 'close', data: { dismiss: 'alert' }) do
+                concat content_tag(:span, '&times;'.html_safe, 'aria-hidden' => true)
+                concat content_tag(:span, 'Close', class: 'sr-only')
+            end)
+            concat message
+        end)
+    end
+    nil
+  end
 end
