066d2530e1293b3fac4953e5a31fb5356396b670
Joseph <jappleton@csumb.edu>
2019-06-20 03:40:09 +0000

    update testing

--- test/controllers/admin_controller_test.rb
+++ test/controllers/admin_controller_test.rb
@@ -1,7 +1,26 @@
 require 'test_helper'
 
 class AdminControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  test "should get terms of service" do
+    get "/tos"
+    assert_response :success
+  end
+  test "should get pricavy policy page" do
+    get "/privacy-policy"
+    assert_response :success
+  end
+  test "should log out" do
+    get "/logout"
+    assert( flash["alert-danger"], "You have logged out.")
+    assert_redirected_to("/")
+  end
+  test 'should get root from nonsense' do
+    get '/wp-admin/login.php'
+    assert_redirected_to("/")
+  end
+  test 'should redirect non-logged-in user' do
+    get "/dashboard"
+    assert( flash["alert-danger"], "Nope." )
+    assert_redirected_to("/")
+  end
 end
